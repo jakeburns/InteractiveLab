52908ca00406ec9803445b21db5aa31d2aa848e1
Jake T. Burns <jake.burns@centre.edu>
2019-03-14 15:41:21 -0400

    wefewef

--- scripts.js
+++ scripts.js
@@ -14,28 +14,39 @@ function(err){
 
 
 var initilize = function(data){
-  var height = 500;
-  var width = 600;
-  var barWidth = (width/data.length)
+  var screens = {width:600, height:500}
+
+
   var colors=d3.scaleOrdinal(d3.schemePaired);
   var margins = {top: 40, right: 50, bottom: 20, left: 30}
 document.getElementsByTagName("p")[0].innerHTML = "0";
   var svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height", height + 60);
+    .attr("width", screen.width)
+    .attr("height", screen.height);
+
+    var width = screen.width - margins.left - margins.right
+    var height = screen.height - margins.top - margins.bottom
+    var barWidth = (width/data.length)
+      console.log(barWidth)
+    var xScale = d3.scaleOrdinal()
+    .domain([d3.range(data[0].grades.length)])
+    .range([0, width]);
+    var yScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([height,0]);
 
     svg.selectAll("rect")
            .data(data[0].grades)
            .enter()
            .append("rect")
            .attr("x", function(d,i){
-             return i * 150 + 45;
+             return xScale(i)
            })
-           .attr("y", function(d){
-             return 495;
+           .attr("y", function(d,i){
+             return yScale(d.grade);
            })
            .attr("height", function(d){
-             return 50;
+             return height-yScale(d.grade);
            })
            .attr("width", barWidth)
             // .attr('transform', 'translate(' + (margins.right+15) + ',' + margins.top + ')')
@@ -43,12 +54,6 @@ document.getElementsByTagName("p")[0].innerHTML = "0";
              return colors(d.name);
            });
 
-           var xScale = d3.scaleLinear()
-                      .domain([0, 3])
-                      .range([5,width]);
-                  var yScale = d3.scaleLinear()
-                      .domain([0, 100])
-                      .range([height,0]);
 
 
           var yAxis = d3.axisLeft(yScale)
